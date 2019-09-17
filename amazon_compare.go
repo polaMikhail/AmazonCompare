62e92561a3f6d078c7f006014324203085fc44bc
pola.alper@gmail.com <pola.alper@gmail.com>
2019-09-17 20:17:51 +0100

    here we go gophers

--- amazon_compare.go
+++ amazon_compare.go
@@ -1,45 +1,51 @@
 package main
 
 import (
-	"encoding/csv"
 	"encoding/json"
-	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"net/url"
+	"sync"
 
 	"github.com/gocolly/colly"
 )
 
-type item struct {
-	ProductUrl string
+type product struct {
+	ProductURL string
 	Name       string
 	Price      string
+	ID         string
 }
+
+var searchURL = []string{"https://www.amazon.co.uk/s?k=", "https://www.amazon.fr/s?k=",
+	"https://www.amazon.es/s?k=", "https://www.amazon.it/s?k=",
+	"https://www.amazon.de/s?k="}
 
 func main() {
 
-	// var name string
-	// flag.StringVar(&name, "opt", "", "Usage")
-
-	flag.Parse()
 	// Instantiate default collector
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.amazon.co.uk", "www.amazon.de", "www.amazon.es", "www.amazon.fr", "www.amazon.it"),
 	)
-	m := make(map[string][]item)
-	var a []item
+	products := make(map[string][]product)
+	searchTerm := "ps4 pro"
+	counter := 0
+
+	var a []product
+
+	// here for each element inside the selector in the on html function we are lookin for a product
 	c.OnHTML("span[data-component-type=\"s-search-results\"]", func(e *colly.HTMLElement) {
 		e.ForEach("div.s-result-list.sg-row > div", func(index int, el *colly.HTMLElement) {
 			name := el.DOM.Find("span.a-size-medium.a-color-base.a-text-normal").Text()
 			price := el.DOM.Find("div.a-section.a-spacing-none.a-spacing-top-small span.a-price > span.a-offscreen").Text()
 			urls := el.ChildAttr("a.a-link-normal.a-text-normal", "href")
 			id := el.Attr("data-asin")
-			product := item{Name: name, Price: price, ProductUrl: urls}
-			if v, found := m[id]; found {
-				m[id] = append(v, product)
+			product := product{Name: name, Price: price, ProductURL: urls, ID: id}
+			if v, found := products[id]; found {
+				products[id] = append(v, product)
 			} else {
-				m[id] = append(a, product)
+				products[id] = append(a, product)
 
 			}
 
@@ -51,33 +57,51 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 	c.OnScraped(func(r *colly.Response) {
-		file, err := os.Create("result.csv")
-		checkError("Cannot create file", err)
-		defer file.Close()
-
-		writer := csv.NewWriter(file)
-		defer writer.Flush()
-		for _, value := range m {
-			out, err := json.Marshal(value)
+		// on scraped will be called once the onHtml fuction is completed
+		fmt.Println("visited")
+		// a counter that increments so we know that we already finished all the iterations in the loop
+		counter++
+		if counter == len(searchURL) {
+			fmt.Println("creating json")
+			// converting the product map into json
+			out, err := json.Marshal(products)
 			if err != nil {
 				panic(err)
 			}
-
-			fmt.Println(string(out))
-			var strArray []string
-			strArray = append(strArray, string(out))
-			err = writer.Write(strArray)
-			checkError("Cannot write to file", err)
+			// output the file into json file
+			err = ioutil.WriteFile("output.json", out, 0644)
 		}
-		fmt.Println(m)
+
 	})
 
-	go c.Visit("https://www.amazon.co.uk/s?k=ps4+pro&ref=nb_sb_noss_1")
-	go c.Visit("https://www.amazon.de/s?k=ps4+pro&ref=nb_sb_noss_1")
-	go c.Visit("https://www.amazon.es/s?k=ps4+pro&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_1")
-	go c.Visit("https://www.amazon.fr/s?k=ps4+pro&__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_1")
-	c.Visit("https://www.amazon.it/s?k=ps4+pro&__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_1")
+	var wg sync.WaitGroup
+	// Tell the 'wg' WaitGroup how many threads/goroutines
+	//   that are about to run concurrently.
+	wg.Add(len(searchURL))
+	for i := 0; i < len(searchURL); i++ {
+		// Spawn a thread for each iteration in the loop.
+		// Pass 'i' into the goroutine's function
+		//   in order to make sure each goroutine
+		//   uses a different value for 'i'.
+		go func(t int) {
 
+			fmt.Println(searchURL[t] + url.QueryEscape(searchTerm))
+			c.Visit(searchURL[t] + url.QueryEscape(searchTerm))
+			// At the end of the goroutine, tell the WaitGroup
+			//   that another thread has completed.
+			defer wg.Done()
+
+		}(i)
+
+	}
+	// Wait for `wg.Done()` to be exectued the number of times
+	//   specified in the `wg.Add()` call.
+	// `wg.Done()` should be called the exact number of times
+	//   that was specified in `wg.Add()`.
+	// If the numbers do not match, `wg.Wait()` will either
+	//   hang infinitely or throw a panic error.
+	wg.Wait()
+	fmt.Println("Finished for loop")
 }
 func checkError(message string, err error) {
 	if err != nil {
